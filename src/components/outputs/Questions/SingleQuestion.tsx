ee7a922ae23874c557832b3bce3cb91fdc4cd53e
Tomer Brown <tomer.brown1997@gmail.coml.com>
2024-11-02 10:14:27 +0200

    Replace static text with accordion for expansion and hints

--- src/components/outputs/Questions/SingleQuestion.tsx
+++ src/components/outputs/Questions/SingleQuestion.tsx
@@ -1,6 +1,11 @@
 import { SingleQuestion as Question } from "../../../hooks/useQuestions";
 import { mapArabicToHebrewLetters } from "../../../mapper/Mapper";
 import {
+  Accordion,
+  AccordionButton,
+  AccordionItem,
+  AccordionPanel,
+  AccordionIcon,
   Card,
   CardBody,
   CardHeader,
@@ -28,43 +33,65 @@ const SingleQuestion = ({ question }: SingleQuestionProps) => {
       </CardHeader>
 
       <CardBody>
-        <Stack divider={<StackDivider />} spacing="4">
-          <Box>
-            <Heading size="xs" textTransform="uppercase">
-              תרגום השאלה:
-            </Heading>
-            <Text pt="2" fontSize="sm">
-              {question.question_in_hebrew !== null
-                ? question.question_in_hebrew
-                : ""}
-            </Text>
-          </Box>
-          <Box>
-            <Heading size="xs" textTransform="uppercase">
-              תשובה בערבית:
-            </Heading>
-            <Text pt="2" fontSize="sm">
-              {question.answer_in_arabic !== null
-                ? question.answer_in_arabic
-                : ""}
-            </Text>
-            <Text pt="2" fontSize="sm">
-              {question.answer_in_arabic !== null
-                ? mapArabicToHebrewLetters(question.answer_in_arabic)
-                : ""}
-            </Text>
-          </Box>
-          <Box>
-            <Heading size="xs" textTransform="uppercase">
-              תרגום התשובה לעברית:
-            </Heading>
-            <Text pt="2" fontSize="sm">
-              {question.answer_in_hebrew !== null
-                ? question.answer_in_hebrew
-                : ""}
-            </Text>
-          </Box>
-        </Stack>
+        <Accordion allowMultiple={true}>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex="1" textAlign="right">
+                  תרגום השאלה:
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              <Text pt="2" fontSize="sm">
+                {question.question_in_hebrew !== null
+                  ? question.question_in_hebrew
+                  : ""}
+              </Text>
+            </AccordionPanel>
+          </AccordionItem>
+
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex="1" textAlign="right">
+                  התשובה בערבית:
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              <Text pt="2" fontSize="sm">
+                {question.answer_in_arabic !== null
+                  ? question.answer_in_arabic
+                  : ""}
+              </Text>
+              <Text pt="2" fontSize="sm">
+                {question.answer_in_arabic !== null
+                  ? mapArabicToHebrewLetters(question.answer_in_arabic)
+                  : ""}
+              </Text>
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex="1" textAlign="right">
+                  תרגום התשובה לעברית:
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              <Text pt="2" fontSize="sm">
+                {question.answer_in_hebrew !== null
+                  ? question.answer_in_hebrew
+                  : ""}
+              </Text>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
       </CardBody>
     </Card>
   );
