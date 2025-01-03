f11bd3ce6a7f52fdab005c7451390fb2e1cd2f2b
Tomer Brown <tomer.brown1997@gmail.coml.com>
2025-01-03 16:11:29 +0200

    Create some basic Youtube player for the youtube page.

--- src/Pages/YoutubeVideoPage.tsx
+++ src/Pages/YoutubeVideoPage.tsx
@@ -1,15 +1,33 @@
 import TemplatePage from "./TemplatePage";
 import Youtube from "../components/Youtube/Youtube";
+import ReactPlayer from "react-player";
+import { Box, VStack } from "@chakra-ui/react";
+import "./YoutubeVideoPage.css";
 
 interface YoutubeVideoPageProps {
-    setArabicText: (text: string) => void;
+  setArabicText: (text: string) => void;
 }
 
-const YoutubeVideoPage = ( {setArabicText}: YoutubeVideoPageProps) => {
+const YoutubeVideoPage = ({ setArabicText }: YoutubeVideoPageProps) => {
   return (
     <div>
       <TemplatePage>
-        <Youtube setArabicText={setArabicText} />
+        <VStack gap={"20px"}>
+          <Youtube setArabicText={setArabicText} />
+          <Box
+            className="video-container"
+            maxWidth={["100%", "500px", "500px", "500px"]}
+          >
+            <ReactPlayer
+              url="https://www.youtube.com/watch?v=RqgvKgXJbOE&t=4s"
+              controls={true}
+              pip={true}
+              light={false}
+              width={"100%"}
+              height={"100%"}
+            />
+          </Box>
+        </VStack>
       </TemplatePage>
     </div>
   );
